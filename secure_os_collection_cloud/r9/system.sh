9fac3ea49488b83f1def478664921019d3b61111
iteasy-managed1 <managed@iteasy.co.kr>
2025-10-16 14:06:19 +0900

    Remove IPv6 disabling from sysctl configuration
    Remove disabling of IPv6 in sysctl configuration.

--- secure_os_collection_cloud/r9/system.sh
+++ secure_os_collection_cloud/r9/system.sh
@@ -59,7 +59,6 @@ configure_sysctl() {
     log_info "configure_sysctl 시작"
     # 내부 표준 최소값(IPv6 off 등) - 필요 시 조정
     cat <<'EOF' > /etc/sysctl.conf
-net.ipv6.conf.all.disable_ipv6 = 1
 net.ipv4.icmp_echo_ignore_broadcasts = 1
 net.ipv4.tcp_rmem = 4096 10000000 16777216
 net.ipv4.tcp_wmem = 4096 65536 16777216
